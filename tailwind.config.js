1e66906addeaa752148eb97c20a1d957e144ebbf
Dominik <dominik.rubroeder@icloud.com>
2022-06-29 18:21:04 +0200

    Add CSS animations and transitions for a better UX

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -11,7 +13,81 @@ module.exports = {
         'app-orange': '#EC755D',
         'app-teal': '#76B5BC',
       },
+      animation: {
+        growHeight: 'growHeight 1s ease-out forwards',
+        growWidth: 'growWidth 1s ease-out forwards',
+        fadeUp: 'fadeUp .6s ease-out forwards',
+        fadeDown: 'fadeDown .6s ease-out forwards',
+        fadeToRight: 'fadeToRight .6s ease-out forwards',
+        fadeToLeft: 'fadeToLeft .6s ease-out forwards',
+      },
+      keyframes: {
+        growHeight: {
+          '0%': { height: '0%' },
+          '100%': { transform: '100%' },
+        },
+        growWidth: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+        fadeUp: {
+          '0%': {
+            transform: 'translateY(60%)',
+            opacity: '0%',
+            visibility: 'hidden',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '100%',
+            visibility: 'visible',
+          },
+        },
+        fadeDown: {
+          '0%': {
+            transform: 'translateY(-60%)',
+            opacity: '0%',
+            visibility: 'hidden',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '100%',
+            visibility: 'visible',
+          },
+        },
+        fadeToRight: {
+          '0%': {
+            transform: 'translateX(-30%)',
+            opacity: '0%',
+            visibility: 'hidden',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '100%',
+            visibility: 'visible',
+          },
+        },
+        fadeToLeft: {
+          '0%': {
+            transform: 'translateX(30%)',
+            opacity: '0%',
+            visibility: 'hidden',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '100%',
+            visibility: 'visible',
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.animation-delay-1000': {
+          'animation-delay': '1s',
+        },
+      });
+    }),
+  ],
 };
